ffc5038d61da61034b18452dcf093b6d0a22ab7b
TDVCool123 <luisandypp@gmail.com>
2024-12-20 03:47:10 -0800

    no buil

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ provider "aws" {
 }
 
 resource "aws_key_pair" "docker-server-ssh" {
-   key_name   = "docker-server-ssh-ec2tfa"
+   key_name   = "docker-server-ssh-ec2tfb"
    public_key = file("docker-server.key.pub")
 }
 
 
 resource "aws_security_group" "docker-server-sg" {
- name        = "docker-server-sg-ec2tfa"
+ name        = "docker-server-sg-ec2tfb"
  description = "Security group allowing SSH and HTTP access"
 
 
